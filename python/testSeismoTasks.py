2d528bc3718f0f1bd05117eb6ec841766f9cf991
tjo <owens@seis.sc.edu>
2019-02-24 15:53:04 -0500

    SeismogramTasks.py contains a series of simple routines that may make life easier when we have actual flowing down the wire.
    testSeismoTasks.py is a set of tests to output results of the routines.
    
    Seems like things work.

---
+++ python/testSeismoTasks.py
@@ -0,0 +1,36 @@
+import sys
+import struct
+import array
+import math
+from SeismogramTasks import *
+
+x = [1,1,1]
+y=x
+z=x
+
+
+
+print(VectorMagnitude(1,1,1))
+print(Magnitude_ThreeC_TimeSeries(x,y,z))
+
+print(Divide_TimeSeries(x,3))
+print(Multiply_TimeSeries(x,3))
+print(Add_TimeSeries(x,3))
+print(Subtract_TimeSeries(x,3))
+
+print(Sum_TimeSeries(x))
+print(RemoveMean_TimeSeries(x))
+
+x = [1,0,0]
+y = [0,0,0]
+
+print(Coordinate_Rotation_2D(1,0,45.))
+print(Rotate_2D_TimeSeries(x,y,45.))
+
+x = [1,1,1]
+y = [1,1,1]
+o = [0,0,0,0,0]
+lo=len(o)
+
+Convolve_TimeSeries(x,3,y,3,o,lo)
+print("output",o)
