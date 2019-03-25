970958a2a95cf37a7d3036a701095c5774c6a524
Philip Crotwell <crotwell@seis.sc.edu>
2019-03-25 19:03:50 -0400

    begin testing new auth command (requires modified ringserver), big try around parseReponse to close connection if read fails

--- python/testDali.py
+++ python/testDali.py
@@ -4,11 +4,13 @@ import asyncio
 import logging
 from datetime import datetime, timedelta
 from array import array
+import jwt
 
 logging.basicConfig(level=logging.DEBUG)
 
 host = "129.252.35.36"
 port = 15003
+port = 15005
 #host = "129.252.35.20"
 #port = 6383
 uri = "wss://www.seis.sc.edu/dragracews/datalink"
@@ -18,10 +20,29 @@ username="dragrace"
 processid=0
 architecture="python"
 
+def encode_auth_token(user_id, secretKey):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    payload = {
+        'exp': datetime.utcnow() + timedelta(days=0, seconds=5),
+        'iat': datetime.utcnow(),
+        'sub': user_id,
+        'wpat': 'XX_.*/MSEED'
+    }
+    encoded = jwt.encode(
+        payload,
+        secretKey,
+        algorithm='HS256'
+    )
+    decoded = jwt.decode(encoded, secretKey, algotithms='HS256')
+    print("decode: {}".format(decoded))
+    return encoded
 
 def doTest(loop):
-    #dali = simpleDali.SocketDataLink(host, port)
-    dali = simpleDali.WebSocketDataLink(uri, verbose=True)
+    dali = simpleDali.SocketDataLink(host, port)
+    #dali = simpleDali.WebSocketDataLink(uri, verbose=True)
     idTask = loop.create_task(dali.id(programname, username, processid, architecture))
     loop.run_until_complete(idTask)
     serverId = idTask.result()
@@ -41,6 +62,14 @@ def doTest(loop):
     # nowTime = datetime.utcnow()
     # print("hptime now: {} {}".format(hptime, simpleDali.datetimeToHPTime(simpleDali.hptimeToDatetime(int(hptime)))))
 
+    # test auth
+    secretKey = '234jkasdf342saf345h6'
+    token = encode_auth_token(username, secretKey)
+    print("token: {}".format(token))
+    authTask = loop.create_task(dali.auth(token))
+    loop.run_until_complete(authTask)
+    print("Auth Resp: {}".format(authTask.result()))
+
     network = "XX"
     station = "TEST"
     location = "00"
@@ -53,11 +82,10 @@ def doTest(loop):
         shortData.append(i)
     msh = simpleMiniseed.MiniseedHeader(network, station, location, channel, starttime, numsamples, samprate)
     msr = simpleMiniseed.MiniseedRecord(msh, shortData)
-    print("before writeMSeed")
-    sendTask = loop.create_task(dali.writeMSeed(msr))
-    loop.run_until_complete(sendTask)
-    #resp = await dali.writeMSeed(msr)
-    print("writemseed resp {}".format(sendTask.result()))
+#    print("before writeMSeed")
+#    sendTask = loop.create_task(dali.writeMSeed(msr))
+#    loop.run_until_complete(sendTask)
+#    print("writemseed resp {}".format(sendTask.result()))
     closeTask = loop.create_task(dali.close())
     loop.run_until_complete(closeTask)
 
