9699dcbe3915cb51f82ba6fd3c5733f2c1b7de5f
unknown <you@example.com>
2019-03-25 16:53:56 -0400

    fixing of compareSendPeakAccel

--- python/peakACC.py
+++ python/peakACC.py
@@ -4,9 +4,13 @@ import struct
 import array
 import math
 from SeismogramTasks import Rotate_2D_TimeSeries,VectorMagnitude
-import datetime
-timestamp = datetime.datetime.utcnow().isoformat() # time need to be...
+from datetime import datetime, timedelta
+import json
+# import simpleDali
+# timestamp = datetime.datetime.utcnow().isoformat() # time need to be...
 # datetime.isoformat()
+# note this time is associated with last element of x,y,z data we get
+# would like to compare first and last of two packets..?
 
 # note: each element of x,y,z makes one vector!
 array_x = [1,2,3,4,500,15,25]
@@ -14,6 +18,9 @@ array_y = [5,6,7,8,32,69,70]
 array_z = [9,10,11,12,47,100,0]
 theta = 20.0
 station = 'PI01'
+start_time = datetime.utcnow()
+time_diff = timedelta(seconds=0.20)
+end_time = start_time + time_diff
 
 # overall : take in raw packets (arrays Z,N,E), do rotation and rest state
 # correction, calculate peak counts over the entire time series, divide
@@ -21,7 +28,7 @@ station = 'PI01'
 # magnitude over a small amount of time (depends on len of arrays given),
 # and sends this mag to ring server
 
-def peakAccelerationCalculation(x,y,z,theta,station,time):
+def peakAccelerationCalculation(x,y,z,theta,station,start_time,end_time):
     # rotation correction
     # First, do coordiate rotation about the y-axis (downtrack). Note the...
     #  Rotate_2D_TimeSeries function has rotation about z, but can put...
@@ -39,12 +46,47 @@ def peakAccelerationCalculation(x,y,z,theta,station,time):
     peakAccel = countsTog(vmag)
     maxAcceljson = {
         "station":station,
-        "time":time,
+        "start_time":start_time,
+        # "start_time":start in MMA8451 line 254
+        "end_time":end_time,
+        # "end_time": last_sample_time in MMA8451 line, also as now
         "MAXACC":max(peakAccel)
      }
     return maxAcceljson
 
-def 
+def compareSendPeakAccel(establishedJson, freshJson, Dali, maxWindow):
+    if freshJson is None:
+        print('freshjson is none')
+    if establishedJson is None:
+        print('established is none')
+        return freshJson
+    if freshJson["end_time"] - establishedJson["start_time"] < maxWindow:
+        print('in time window')
+        establishedJson["end_time"] = freshJson["end_time"]
+        if freshJson["MAXACC"] > establishedJson["MAXACC"]:
+            establishedJson["MAXACC"] = freshJson["MAXACC"]
+            # if "freshly calculated" maxAcceljson has larger MAXCC than
+            # "already established" json, then establsihed takes on values of
+            # freshjson
+        else:
+            None
+        # compare the peak accel
+        # update prevJson to have end_time of newJson, as well as newJson's
+        # MAXCC
+        return establishedJson
+    else:
+        establishedJson["start_time"] = establishedJson["start_time"].isoformat()
+        establishedJson["end_time"] = establishedJson["end_time"].isoformat()
+        if Dali is not None:
+            # send establishedJson to ring server
+            Dali.writeJsonToDatalink(establishedJson)
+            print('sending to ringserver')
+        else:
+            print(json.dumps(establishedJson,indent = 4))
+        return freshJson
+        # if time between samples is >= 0.25s, then make establishedJson take
+        # on values of freshJson, so freshJson will can be compared to
+
     # time = datetime.datetime.utcnow()
     # time_2 = datetime.datetime.utcnow()
     # time_3 = time - time_2
@@ -61,18 +103,12 @@ def
     #     prevTime = maxAcceljson["time"]
     #     # if now - datetime.datetime.timedelta(seconds=0.25):
 
-
-
-
-    # Create dictionary that return
-    # peakAcceljson = {"station":s, "time": starttime, "maxacc": max(peakAccel)}
-
-
 # magnitude time series
 # function to take magnitude of rotated and rest state corrected time ...
 # series
 # input = rotate_array_x,array_y,new_array_z
 # output = vmag
+
 def Magnitude_ThreeC_TimeSeries_jake(x,y,z):
     nptsx=len(x)
     nptsy=len(y)
@@ -113,10 +149,14 @@ def rest_state_correction(rotate_array_z, rest_factor_z):
         correct.append(i+rest_factor_z)
     return correct
 
-print(peakAccelerationCalculation(array_x,array_y,array_z,theta,station,timestamp))
 
-
+# print('---')
+# print(sendPeakAccel(newJson))
 # timedelta should be about 0.25-0.5 s
+
+freshJson = peakAccelerationCalculation(array_x,array_y,array_z,theta,station,start_time,end_time)
+print(freshJson)
+# establishedJson =
 # need an IP catcher, config listener + thrower, and make a dictionary...
 #  that makes info dictionary for each heat (time, max accel, )
 # Theta is found from config listener + thrower from ring server
