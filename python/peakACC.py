9a7df768c4c5d3c1f09b55eb0d180ede5b2b0de7
Jake T. Burstein <jacobtb@email.sc.edu>
2019-03-27 12:22:10 -0400

    cleaning up

--- python/peakACC.py
+++ python/peakACC.py
@@ -1,32 +1,21 @@
-# Making peak acceleration stuff
+# Calculate Peak Acceleration
 import sys
 import struct
 import array
 import math
 from SeismogramTasks import Rotate_2D_TimeSeries,VectorMagnitude
 from datetime import datetime, timedelta
-import json 
-# import simpleDali
-# timestamp = datetime.datetime.utcnow().isoformat() # time need to be...
-# datetime.isoformat()
-# note this time is associated with last element of x,y,z data we get
-# would like to compare first and last of two packets..?
+import json
 
 # note: each element of x,y,z makes one vector!
 array_x = [1,2,3,4,500,15,25]
 array_y = [5,6,7,8,32,69,70]
 array_z = [9,10,11,12,47,100,0]
-theta = 20.0
-station = 'PI01'
-start_time = datetime.utcnow()
-time_diff = timedelta(seconds=0.20)
-end_time = start_time + time_diff
-
-# overall : take in raw packets (arrays Z,N,E), do rotation and rest state
-# correction, calculate peak counts over the entire time series, divide
-#  time series by factor to convert into g's (4069 = 1g?), returns the g's
-# magnitude over a small amount of time (depends on len of arrays given),
-# and sends this mag to ring server
+# theta = 20.0
+# station = 'PI01'
+# start_time = datetime.utcnow()
+# time_diff = timedelta(seconds=0.20)
+# end_time = start_time + time_diff
 
 def peakAccelerationCalculation(x,y,z,theta,station,start_time,end_time):
     # rotation correction
@@ -66,13 +55,10 @@ def compareSendPeakAccel(establishedJson, freshJson, Dali, maxWindow):
         if freshJson["MAXACC"] > establishedJson["MAXACC"]:
             establishedJson["MAXACC"] = freshJson["MAXACC"]
             # if "freshly calculated" maxAcceljson has larger MAXCC than
-            # "already established" json, then establsihed takes on values of
+            # "already established" json, then establsihed takes on MAXACC of
             # freshjson
         else:
             None
-        # compare the peak accel
-        # update prevJson to have end_time of newJson, as well as newJson's
-        # MAXCC
         return establishedJson
     else:
         establishedJson["start_time"] = establishedJson["start_time"].isoformat()
@@ -84,31 +70,12 @@ def compareSendPeakAccel(establishedJson, freshJson, Dali, maxWindow):
         else:
             print(json.dumps(establishedJson,indent = 4))
         return freshJson
-        # if time between samples is >= 0.25s, then make establishedJson take
-        # on values of freshJson, so freshJson will can be compared to
-
-    # time = datetime.datetime.utcnow()
-    # time_2 = datetime.datetime.utcnow()
-    # time_3 = time - time_2
-    # return time_3
-    # NOTE: can do:  if now - prevTime == 0.25 update maxAcceljson dictionary
-
-
-    # need to make station and time configuerable/changeable within function
-    # need to then send this dictionary every 0.25-0.5 sec
-    # make new maxAcceljson every 0.25 secs
-    # def sendPeakAccel():
-    #     now = datetime.datetime.utcnow()
-    #     prevAcc = maxAcceljson["MAXACC"]
-    #     prevTime = maxAcceljson["time"]
-    #     # if now - datetime.datetime.timedelta(seconds=0.25):
 
 # magnitude time series
 # function to take magnitude of rotated and rest state corrected time ...
 # series
 # input = rotate_array_x,array_y,new_array_z
 # output = vmag
-
 def Magnitude_ThreeC_TimeSeries_jake(x,y,z):
     nptsx=len(x)
     nptsy=len(y)
@@ -148,15 +115,3 @@ def rest_state_correction(rotate_array_z, rest_factor_z):
     for i in rotate_array_z:
         correct.append(i+rest_factor_z)
     return correct
-
-
-# print('---')
-# print(sendPeakAccel(newJson))
-# timedelta should be about 0.25-0.5 s
-
-freshJson = peakAccelerationCalculation(array_x,array_y,array_z,theta,station,start_time,end_time)
-print(freshJson)
-# establishedJson =
-# need an IP catcher, config listener + thrower, and make a dictionary...
-#  that makes info dictionary for each heat (time, max accel, )
-# Theta is found from config listener + thrower from ring server
