38c98344101483d81a5de9167d9a1ab71f12910a
Jake T. Burstein <jacobtb@email.sc.edu>
2019-04-01 10:23:11 -0400

    testing

--- python/peakAccelTest.py
+++ python/peakAccelTest.py
@@ -1,6 +1,40 @@
-from SeismogramTasks import VectorMagnitude, Magnitude_ThreeC_TimeSeries, Rotate_2D_TimeSeries,
+from SeismogramTasks import VectorMagnitude, Rotate_2D_TimeSeries, Coordinate_Rotation_2D
+from peakACC import Magnitude_ThreeC_TimeSeries_jake
 import math
 
 x = [1.2, 1.5, 0.0, 0.4, -0.3, 1.5]
-y = [0.3, 0.2. 0.7, 0.3, 0.0, -0.5]
+y = [0.3, 0.2, 0.7, 0.3, 0.0, -0.5]
 z = [-0.1, 1.2, 1.4, 1.0, 1.1, 0.2]
+
+theta = [110.0, 45.0, -45.0, 20.0, -20.0, 30.0]
+
+# Rotate xyz array, find vector mag
+def maxaccel(x,y,z,theta):
+    # Rotate
+    x_prime = []
+    z_prime = []
+    for i,j,k in x,z,theta:
+        r = Coordinate_Rotation_2D(i, j, k)
+        x_prime.append(r[0])
+        z_prime.append(r[1])
+
+    # find vector mag
+    # vmag = Magnitude_ThreeC_TimeSeries_jake(x_prime,z_prime,y)
+    # ACCjson = {
+    #     "x": x_prime,
+    #     "y": y,
+    #     "z": z_prime,
+    #     "theta": theta,
+    #     "VMAG": vmag
+    # }
+    # return ACCjson
+    return len(x_prime),len(z_prime)
+
+v = maxaccel(x,y,z,theta)
+print(v)
+#
+# v = maxaccel(x,y,z,110.0)
+# # print(v)
+#
+# magnitude = v["VMAG"]
+# print(magnitude)
