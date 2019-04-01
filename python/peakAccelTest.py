408b67a7055fb796212d9d04b611d75a7a99aff8
Jake T. Burstein <jacobtb@email.sc.edu>
2019-04-01 09:32:19 -0400

    pop quiz stuff

---
+++ python/peakAccelTest.py
@@ -0,0 +1,6 @@
+from SeismogramTasks import VectorMagnitude, Magnitude_ThreeC_TimeSeries, Rotate_2D_TimeSeries,
+import math
+
+x = [1.2, 1.5, 0.0, 0.4, -0.3, 1.5]
+y = [0.3, 0.2. 0.7, 0.3, 0.0, -0.5]
+z = [-0.1, 1.2, 1.4, 1.0, 1.1, 0.2]
