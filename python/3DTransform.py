f0fe2bf01dfa497289ea79c99c676b916fda6f33
Jake T. Burstein <jacobtb@email.sc.edu>
2019-04-03 13:02:58 -0400

    confirmation of vp.rotate is valid

--- python/3DTransform.py
+++ python/3DTransform.py
@@ -5,22 +5,34 @@ import array
 import numpy as np
 import vpython as vp # pip install vpython
 
-v1 = vp.vector(0,0,1) # creates a vector of floating points [0.0,0.0,1.0]
+v1 = vp.vector(1,0,0) # creates a vector of floating points [0.0,0.0,1.0]
 # print(v1)
 # print(type(v1)) # class 'vpython.cyvector.vector'
 # print(v1.x, v1.y, v1.z) # returns each component
 
 # ROTATE
-degrees_rotate = 20.0
-theta = vp.radians(degrees_rotate) # theta is radians of measured degrees
+degrees_from_vertical = 20.0
+degrees_rotate = degrees_from_vertical + 90.0
+theta = math.radians(degrees_rotate) # theta is radians of measured degrees
 v2 = vp.rotate(v1,angle=theta,axis=vp.vector(0,1,0))
-print("vp rotate {}".format(v2))
+print("vp rotate: {}".format(v2))
 # print(v2) # outputs a rotation of v1 about the y-axis by theta radians
 
+# gabby stuff
 A = np.array([[0.94, 0.0, 0.34],[0,1,0],[-0.342,0.0,0.94]])  # coordinate transform matrix for 20 deg
 v_prime = np.array([1,0,0])
 v = np.matmul(A,v_prime)
-print("matlab rotate {}".format(v))
+print("matlab rotate: {}".format(v))
+
+# making own CS transform of 20 deg from vertical = 110 deg roration 
+A_jake = np.array([[math.cos(math.radians(110)),math.cos(math.radians(0.0)),math.cos(math.radians(200.0))],
+                [0,math.cos(math.radians(0)),0],
+                [math.cos(math.radians(20)),0,math.cos(math.radians(110))]])
+# coordiate rotation matrix of 110 deg counter clockwise about y axis
+print(A_jake)
+v_prime_jake = np.array([1,0,0]) # vector in prime CS aligned with x' axis
+v_jake = np.matmul(A_jake,v_prime_jake)
+print("v_jake rotate: {}".format(v_jake))
 
 
 # xlocal,ylocal,zlocal are components of a vector in the
