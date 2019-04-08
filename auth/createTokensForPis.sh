5f8085208199d914765653b10821aa43df2ee0a9
Philip Crotwell <crotwell@seis.sc.edu>
2019-04-08 18:27:32 -0400

    one token for all MAXACC and MSEED as we do not know our station at startup

--- auth/createTokensForPis.sh
+++ auth/createTokensForPis.sh
@@ -6,3 +6,7 @@ for i in {1..9}
 do
    python3 ../python/createToken.py -p XX_PI0${i}/IP -s ./secret.key -u dragrace -o pi0${i}_token.jwt --ed ${DAYS}
 done
+
+python3 ../dragrace/python/createToken.py -p 'XX(.|_).*/((IP)|(MAXACC)|(MSEED))' -s ./secret.key -u dragrace -o mseed_token.jwt --ed ${DAYS}
+
+
