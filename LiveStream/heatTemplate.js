63f0ac426f0c20d399d8a0cba595907a72ed3308
Gabby <herringabrielle@gmail.com>
2019-04-22 17:02:41 -0400

    ok

--- LiveStream/heatTemplate.js
+++ LiveStream/heatTemplate.js
@@ -21,10 +21,10 @@ fetch('http://www.seis.sc.edu/dragrace/www/results/data/Friday/TopFuel/Heat1/tri
     return response.json();
   })
   .then(function(trig) {
-    d3.select("div.currentRace").select("div.start_time").text(`Start Time = ${`trig.start`}`);
-    d3.select("div.currentRace").select("div.end_time").text(`End Time = ${`trig.end`}`);
-    d3.select("div.currentRace").select("div.race_class").text(`Class = ${`trig.class`}`);
-    d3.select("div.currentRace").select("div.race_heat").text(`Heat = ${`trig.heat`}`);
-    d3.select("div.currentRace").select("div.dutyOfficer").text(`Duty Officer = ${`trig.officer`}`);
+    d3.select("div.currentRace").select("div.start_time").text(`Start Time = ${trig.start}`);
+    d3.select("div.currentRace").select("div.end_time").text(`End Time = ${trig.end}`);
+    d3.select("div.currentRace").select("div.race_class").text(`Class = ${trig.class}`);
+    d3.select("div.currentRace").select("div.race_heat").text(`Heat = ${trig.heat}`);
+    d3.select("div.currentRace").select("div.dutyOfficer").text(`Duty Officer = ${trig.officer}`);
 
   })
