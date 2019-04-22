91c1c616a33759a34e624228a7074d00fccffbf1
Gabby <herringabrielle@gmail.com>
2019-04-22 16:46:38 -0400

    error fix

--- LiveStream/heatTemplate.js
+++ LiveStream/heatTemplate.js
@@ -3,7 +3,6 @@ let datalink = seisplotjs.datalink
 
 //let wp = require('seisplotjs-waveformplot');
 // this global comes from the seisplotjs_waveformplot standalone js
-let wp = seisplotjs.waveformplot;
 let d3 = seisplotjs.d3;
 let moment = seisplotjs.moment;
 
@@ -22,10 +21,10 @@ fetch('http://www.seis.sc.edu/dragrace/www/results/data/Friday/TopFuel/Heat1/tri
     return response.json();
   })
   .then(function(trig) {
-    wp.d3.select("div.currentRace").select("div.start_time").attr(`title`,`Start Time = ${`start`}`);
-    wp.d3.select("div.currentRace").select("div.end_time").attr(`title`,`End Time = ${`end`}`);
-    wp.d3.select("div.currentRace").select("div.race_class").attr(`title`,`Class = ${`class`}`);
-    wp.d3.select("div.currentRace").select("div.race_heat").attr(`title`,`Heat = ${`heat`}`);
-    wp.d3.select("div.currentRace").select("div.dutyOfficer").attr(`title`,`Duty Officer = ${`officer`}`);
+    d3.select("div.currentRace").select("div.start_time").attr(`title`,`Start Time = ${`start`}`);
+    d3.select("div.currentRace").select("div.end_time").attr(`title`,`End Time = ${`end`}`);
+    d3.select("div.currentRace").select("div.race_class").attr(`title`,`Class = ${`class`}`);
+    d3.select("div.currentRace").select("div.race_heat").attr(`title`,`Heat = ${`heat`}`);
+    d3.select("div.currentRace").select("div.dutyOfficer").attr(`title`,`Duty Officer = ${`officer`}`);
 
   })
