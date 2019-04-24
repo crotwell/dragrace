07eb98370b755d4c5520781d8969df566c715850
Gabby <herringabrielle@gmail.com>
2019-04-24 17:50:30 -0400

    Edits to equalizer and results page

--- LiveStream/heatTemplate.js
+++ LiveStream/heatTemplate.js
@@ -15,34 +15,50 @@ let trig = {
   "heat":"one",
   "officer":"Gabby"
 };
-let day = "Friday";
-fetch(`http://www.seis.sc.edu/dragrace/www/results/data/${day}/classnames.json`)
+let day = "Wednesday";
+fetch(`http://www.seis.sc.edu/dragrace/www/results/${day}/classnames.json`)
   .then(function(response){
     return response.json();
   })
   .then(function(classnames) {
     for (let c of classnames){
 
-      fetch(`http://www.seis.sc.edu/dragrace/www/results/data/${day}/${c}/heatnames.json`)
+      fetch(`http://www.seis.sc.edu/dragrace/www/results/${day}/${c}/heatnames.json`)
         .then(function(response){
           return response.json();
         })
         .then(function(heatnames) {
             for (let h of heatnames){
 
-          fetch(`http://www.seis.sc.edu/dragrace/www/results/data/${day}/${c}/${h}/results.json`)
+          fetch(`http://www.seis.sc.edu/dragrace/www/results/${day}/${c}/${h}/results.json`)
             .then(function(response){
               return response.json();
             })
-            .then(function(trig) {
-              d3.select("div.currentRace").select("div.start_time").text(`Start Time = ${trig.start}`);
-              d3.select("div.currentRace").select("div.end_time").text(`End Time = ${trig.end}`);
-              d3.select("div.currentRace").select("div.race_class").text(`Class = ${trig.class}`);
-              d3.select("div.currentRace").select("div.race_heat").text(`Heat = ${trig.heat}`);
-              d3.select("div.currentRace").select("div.dutyOfficer").text(`Duty Officer = ${trig.officer}`);
+            .then(function(result) {
+              d3.select("div.currentRace").select("div.start_time").text(`Start Time = ${result.Trigger_Info.startTime}`);
+              d3.select("div.currentRace").select("div.end_time").text(`End Time = ${result.Trigger_Info.endTime}`);
+              d3.select("div.currentRace").select("div.race_class").text(`Class = ${result.Trigger_Info.class}`);
+              d3.select("div.currentRace").select("div.race_heat").text(`Heat = ${result.Trigger_Info.heat}`);
+              d3.select("div.currentRace").select("div.dutyOfficer").text(`Duty Officer = ${result.Trigger_Info.dutyOfficer}`);
+              let datasetNow = [result.peakACC_FL,result.peakACC_NL,result.peakACC_NR,result.peakACC_FR];
+              d3.select("div.currentRace").select("div.maxacc").text(`Ground Acceleration = ${datasetNow[1]}`);
+              let equalizer = new Equalizer("div.equalizer");
+              let eqMap = createEqualizerMap(result)
+              equalizer.updateEqualizer(eqMap);
+
+
 
             });
           }
         });
       }
     });
+
+    createEqualizerMap = function(result){
+      let dataset=new Map();
+      dataset.set("FL",{'station':'FL','maxacc':result.peakACC_FL});
+      dataset.set("NL",{'station':'NL','maxacc':result.peakACC_NL});
+      dataset.set("NR",{'station':'NR','maxacc':result.peakACC_NR});
+      dataset.set("FR",{'station':'FR','maxacc':result.peakACC_FR});
+      return dataset;
+    }
