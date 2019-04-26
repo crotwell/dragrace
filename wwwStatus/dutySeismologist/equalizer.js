ae220c679fd31d303e61197f41c4c9aba5c361ca
Gabby <herringabrielle@gmail.com>
2019-04-26 17:42:45 -0400

    2.0

--- wwwStatus/dutySeismologist/equalizer.js
+++ wwwStatus/dutySeismologist/equalizer.js
@@ -8,7 +8,7 @@ class Equalizer{
     this.width = 330 - this.margin.left - this.margin.right;
     this.height = 400 - this.margin.top - this.margin.bottom;
     this.yScale = d3.scaleLinear()
-    .domain([0,(1.5)])
+    .domain([0,(2.0)])
     .range([this.height, 0]);
     console.log(`yscaletest ${this.yScale(1)}`)
     this.yAxis = d3.axisLeft(this.yScale).ticks(10, "0.1f");
