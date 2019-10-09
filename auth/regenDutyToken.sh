4c80199ee0b5235e0508ff51bd991f709c653656
Philip Crotwell <crotwell@seis.sc.edu>
2019-10-09 11:09:47 -0400

    fix bash

--- auth/regenDutyToken.sh
+++ auth/regenDutyToken.sh
@@ -1,3 +1,3 @@
-/bin/bash
+#!/bin/bash
 
 /usr/bin/python3 /home/geo/Auth/dragrace/python/createToken.py -p '/MTRIG' -s /home/geo/Auth/auth/secret.key -u dragrace -o /home/geo/Auth/www/dutytoken.jwt --em 70 >> /home/geo/Auth/regenToken.log 2>&1
