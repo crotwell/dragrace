04b9faa5e60ca594daa278d66f5563edb7e4acda
Philip Crotwell <crotwell@seis.sc.edu>
2019-11-18 10:00:46 -0500

    create master summary file of all triggers for easier downloading

---
+++ python/triggerSummary.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import json
+import os
+
+days = [ 'Friday', 'Saturday', 'Sunday', 'Monday']
+
+resultsFile = "results/{}/{}/{}/results.json"
+classNamesFile = "results/{}/classnames.json"
+heatNamesFile = "results/{}/{}/heatnames.json"
+allResultsFile = "results/allResults.json"
+allResults = []
+
+for d in days:
+    if os.path.exists(classNamesFile.format(d)):
+        with open(classNamesFile.format(d)) as cfile:
+            classNames = json.load(cfile)
+            # first item is day name
+            for c in classNames[1:]:
+                with open(heatNamesFile.format(d, c)) as hfile:
+                    heats = json.load(hfile)
+                    for h in heats:
+                        with open(resultsFile.format(d,c,h)) as rfile:
+                            result = json.load(rfile)
+                            allResults.append(result)
+with open(allResultsFile, 'w') as f:
+    json.dump(allResults, f)
